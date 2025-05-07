2273cf000df84cdc970107823aa9374ca609899d
Erentalhaokan <talhaokan53@gmail.com>
2025-05-08 00:00:03 +0300

    CalmaListesi.java

--- CalmaListesi.java
+++ CalmaListesi.java
@@ -30,9 +30,23 @@ public class CalmaListesi {
         Sarkı sarkı1 = new Sarkı("Unuttun mu beni", "Sezen Aksu");
         Sarkı sarkı2 = new Sarkı("Tükeneceğiz", "Sezen Aksu");
         Sarkı sarkı3 = new Sarkı("Firuze", "Sezen Aksu");
+        Sarkı sarkı4 = new Sarkı("Gündüzüm seninle","Ferdi Özbeğen");
+        Sarkı sarkı5 = new Sarkı("Olsun","Sertab Erener");
+        Sarkı sarkı6 = new Sarkı("Yanarım","Sertab Erener");
+        Sarkı sarkı7 = new Sarkı("Kül","Cem Adrian");
+        Sarkı sarkı8 = new Sarkı("Derinlerde","Cem Adrian");
+        Sarkı sarkı9 = new Sarkı("Kapkara","Sıla");
+        Sarkı sarkı10 = new Sarkı("Vurgunum","Ahmet Kaya");
         benimListem.sarkiEkle(sarkı1);
         benimListem.sarkiEkle(sarkı2);
         benimListem.sarkiEkle(sarkı3);
+        benimListem.sarkiEkle(sarkı4);
+        benimListem.sarkiEkle(sarkı5);
+        benimListem.sarkiEkle(sarkı6);
+        benimListem.sarkiEkle(sarkı7);
+        benimListem.sarkiEkle(sarkı8);
+        benimListem.sarkiEkle(sarkı9);
+        benimListem.sarkiEkle(sarkı10);
 
         benimListem.calmaListesiniGoster();
     }
